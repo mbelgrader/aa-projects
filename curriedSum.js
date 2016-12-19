c505b911b1e0aa24b962e35d1c274f03e607f556
Maria Belgrader <mbelgrader5@gmail.com>
2016-12-18 16:44:16 -0800

    Add curry to Function.prototype

--- curriedSum.js
+++ curriedSum.js
@@ -18,5 +18,49 @@ function curriedSum(numArgs) {
   return _curriedSum;
 }
 
-console.log(curriedSum(3)(4)(20)(6)); // 30
-console.log(curriedSum(2)(4)(20)); // 24
+// console.log(curriedSum(3)(4)(20)(6)); // 30
+// console.log(curriedSum(2)(4)(20)); // 24
+
+Function.prototype.curry = function(numArgs) {
+  const args = [];
+  const that = this;
+
+  function _curried(arg) {
+    args.push(arg);
+
+    if (args.length === numArgs)
+      return that.apply(null, args);
+    else
+      return _curried;
+  }
+
+  return _curried;
+};
+
+function sumIt() {
+  let result = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+  return result;
+}
+
+// console.log(sumIt.curry(3)(4)(20)(6));
+
+Function.prototype.curry2 = function(numArgs) {
+  const args = [];
+  const that = this;
+
+  function _curried(arg) {
+    args.push(arg);
+
+    if (args.length === numArgs)
+      return that(...args);
+    else
+      return _curried;
+  }
+
+  return _curried;
+};
+
+console.log(sumIt.curry2(3)(4)(20)(6));
